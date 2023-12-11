ca50f5651f19ce99eb1c4970c24dd3e288c9f33c
Sam Monballiu <samsonmonballiu@hotmail.com>
2023-12-11 20:11:58 +0100

    update styling

--- frontend/src/components/NoteDetail.tsx
+++ frontend/src/components/NoteDetail.tsx
@@ -2,6 +2,7 @@ import { Note } from "../models";
 import { FC } from "react";
 import Markdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
+import styles from "./NoteDetail.module.scss";
 
 interface Props {
   note: Note;
@@ -9,10 +10,13 @@ interface Props {
 
 export const NoteDetail: FC<Props> = ({ note }) => {
   return (
-    <>
+    <div className={styles.container}>
       <h1>{note.subject}</h1>
       <h4>{note.createdOn?.toLocaleDateString()}</h4>
-      <Markdown rehypePlugins={[rehypeHighlight]}>{note.content}</Markdown>
-    </>
+
+      <Markdown className={styles.content} rehypePlugins={[rehypeHighlight]}>
+        {note.content}
+      </Markdown>
+    </div>
   );
 };
